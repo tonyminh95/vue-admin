5b28f92405ce3aefa09a2293e7863234fc21bf9b
tonyminh95 <minhbui12121995@gmail.com>
2019-07-05 23:59:44 +0900

    refactor code

--- src/main.js
+++ src/main.js
@@ -6,10 +6,9 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-// Vue.use(ArgonDashboard)jjkljljlkjjklkjkljkhjkhjhjhj
+// Vue.use(ArgonDashboard)
 
-new Vue({hkjhkjhkjhjhhjkhjkhhj
+new Vue({
     router,
     render: h => h(App),
 }).$mount('#app')
-hhhjkhkhj
