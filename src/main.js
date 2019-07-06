c7f1a674a244ffc5f43cac129a216f3eaac91770
tonyminh95 <minhbui12121995@gmail.com>
2019-07-06 22:16:36 +0900

    use buefy

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 // import './registerServiceWorker'
 // import ArgonDashboard from './plugins/argon-dashboard'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
